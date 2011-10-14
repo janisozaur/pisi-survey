f39f50030928c322c281725ea2bd54446ac4bba2
Michał Janiszewski <janisozaur@gmail.com>
2011-10-14 22:22:30 +0200

    import project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PisiSurvey::Application.config.secret_token = '0776bd1e04d4832979f1f9c7dd9877060c8640c43fb495738235367338805747369059e83e630ca0163902c84bd7b6d0e8ab6f526ad1eae42404a39ad8d1c738'
