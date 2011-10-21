46bbbff08378fc9921fdfdcd2e5dd7a75ae67ff4
Michał Janiszewski <janisozaur@gmail.com>
2011-10-21 23:15:47 +0200

    add type b question controller

---
+++ app/controllers/type_b_question_controller.rb
@@ -0,0 +1,21 @@
+class TypeBQuestionController < ApplicationController
+  def index
+    @questions = TypeBQuestion.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
